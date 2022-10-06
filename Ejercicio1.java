01da767934ef6403410cf4952eaf8a08335cd077
Nuria Rueda <ruedareinanuria@gmail.com>
2022-10-06 09:51:34 +0200

    add exercises

---
+++ Ejercicio1.java
@@ -0,0 +1,7 @@
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        System.out.print("Nuria Rueda Reina");
+        System.out.print("Rincón de la Victoria");
+        System.out.print( "671242817");
+    }
+}
