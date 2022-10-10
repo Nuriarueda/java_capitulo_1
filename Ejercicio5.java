8af929fc26c6589aaea968376844760f319588c4
Nuria Rueda <ruedareinanuria@gmail.com>
2022-10-10 10:33:30 +0200

    ejercicio 5,6 y 7 added

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -2,14 +2,22 @@ import java.util.Scanner;
 public class Ejercicio5{
     public static void main(String[]args){
         Scanner sc = new Scanner (System.in);
-        System.out.println("lunes\tmartes\tmiercoles\tjueves\tviernes");
-        System.out.println("======\t======\t=========\t======\t=======");
-        System.out.println("PROG\tS.INFO\tE.DES\tPROG\tFOL");
-        System.out.println("PROG\tS.INFO\tE.DES\tPROG\tFOL");
-        System.out.println("L.MAR\tS.INFO\tPROG\tL.MAR\tFOL");
-        System.out.println("RECREO\tRECREO\tRECREO\tRECREO\tRECREO");
-        System.out.println("L.MAR\tPROG\tPROG\tL.MAR\tS.INFO");
-        System.out.println("B.DATOS\tPROG\tB.DATOS\tB.DATOS\tS.INFO");
-        System.out.println("B.DATOS\tE.DES\tB.DATOS\tB.DATOS\tS.INFO");
+        String rojo = "\033[31m"; 
+        String verde = "\033[32m"; 
+        String naranja = "\033[33m"; 
+        String azul = "\033[34m"; 
+        String morado = "\033[35m";
+        String celeste = "\033[36m";
+        String blanco = "\033[37m";
+        System.out.println("Lunes\tMartes\tMierc.\tJueves\tViernes");
+        System.out.println("======\t======\t======\t======\t======");
+        System.out.println(verde+"PROG"+morado+"\tS.INFO"+azul+"\tE.DES"+verde+"\tPROG"+blanco+"\tFOL");
+        System.out.println(verde+"PROG"+morado+"\tS.INFO"+azul+"\tE.DES"+verde+"\tPROG"+blanco+"\tFOL");
+        System.out.println(celeste+"L.MAR"+morado+"\tS.INFO"+verde+"\tPROG"+celeste+"\tL.MAR"+blanco+"\tFOL");
+        System.out.println(naranja+"RECREO\tRECREO\tRECREO\tRECREO\tRECREO");
+        System.out.println(celeste+"L.MAR"+verde+"\tPROG"+verde+"\tPROG"+celeste+"\tL.MAR"+morado+"\tS.INFO");
+        System.out.println(rojo+"B.DATOS"+verde+"\tPROG"+rojo+"\tB.DATOS"+rojo+"\tB.DATOS"+morado+"\tS.INFO");
+        System.out.println(rojo+"B.DATOS"+azul+"\tE.DES"+rojo+"\tB.DATOS"+rojo+"\tB.DATOS"+morado+"\tS.INFO");
     }
 } 
+
