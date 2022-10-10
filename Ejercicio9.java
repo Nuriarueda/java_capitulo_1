8038b1797868219fb2689d91ea8eb2a9dceefa48
Nuria Rueda <ruedareinanuria@gmail.com>
2022-10-10 12:44:06 +0200

    ejercicios 8 y 9

---
+++ Ejercicio9.java
@@ -0,0 +1,28 @@
+public class Ejercicio9{
+    public static void main(String[]args){
+        System.out.println("**                       **");
+        System.out.println("****                   ****");
+        System.out.println("******               ******");
+        System.out.println("********           ********");
+        System.out.println("**********       **********");
+        System.out.println("************   ************");
+        System.out.println("************   ************");
+        System.out.println("************   ************");
+        System.out.println("************   ************");
+        System.out.println("************   ************");
+        System.out.println("************   ************");
+        System.out.println("************   ************");
+        System.out.println("************   ************");
+        System.out.println("************   ************");
+        System.out.println("************   ************");
+        System.out.println("************   ************");
+        System.out.println("************   ************");
+        System.out.println("************   ************");
+        System.out.println("**********       **********");
+        System.out.println("********           ********");
+        System.out.println("******               ******");
+        System.out.println("****                   ****");
+        System.out.println("**                       **");
+    }
+} 
+
